8c82a1c5a8259712b2af892adb4a5fb2484e42ad
橋本 寛 <hiroshi_hashimoto@hashimoto-no-MacBook-Pro.local>
2013-06-29 14:09:26 +0900

    デプロイ設定

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,1 +1,20 @@
 #!/bin/sh
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+  echo "This is a pull request. No deployment will be done."
+  exit 0
+fi
+if [ "$TRAVIS_BRANCH" = "develop" ]
+then
+  wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+  git remote add heroku git@heroku.com:sakurazuka-app.git
+  echo "Host heroku.com" >> ~/.ssh/config
+  echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+  echo "   CheckHostIP no" >> ~/.ssh/config
+  echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+  heroku keys:clear
+  yes | heroku keys:add
+  yes | git push heroku master
+  heroku run rake db:migrate
+  heroku restart
+fi
